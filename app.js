d920d0c4b8d6fe798c547052052ec6ca2236b764
Johnathon Broodie <johnathon.broodie@nhs.net>
2018-11-08 17:21:05 +0000

    Port basic HTML from C2S site layout

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+app.use('/assets', express.static(path.join(__dirname, '/node_modules')));
+app.use('/nhsuk-frontend', express.static(path.join(__dirname, '/node_modules/nhsuk-frontend')));
 
 app.listen(8081);
