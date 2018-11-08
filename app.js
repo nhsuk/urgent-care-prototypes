3a1a1ee998d22e9eb363df399fe2e73e2e80d481
Johnathon Broodie <johnathon.broodie@nhs.net>
2018-11-08 17:55:58 +0000

    Fixup nunjucks, add header and footer

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nunjucks = require('nunjucks');
 
-app.use(express.static('public'));
 app.use('/assets', express.static(path.join(__dirname, '/node_modules')));
-app.use('/nhsuk-frontend', express.static(path.join(__dirname, '/node_modules/nhsuk-frontend')));
+app.use('/nhsuk-frontend', express.static(path.join(__dirname, '/node_modules/nhsuk-frontend/packages')));
+
+var appViews = [
+    path.join(__dirname, '/node_modules/nhsuk-frontend/packages/'),
+    path.join(__dirname, '/public/')
+  ]
+
+nunjucks.configure(appViews, {
+    autoescape: true,
+    express: app,
+    noCache: true,
+    watch: true
+})
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
 
 app.listen(8081);
