7222ace5d3acb5ecf7a0d278fa500e0195b5cbf3
Prem Bonsall <pbonsall@Development.HealthyChoices.gsi.gov.uk>
2018-11-29 16:09:07 +0000

    config for run watch and removal of files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-sass.compiler = require('node-sass');
+var rename = require("gulp-rename");
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('sass', function () {
-  return gulp.src('./public/styles/*.scss')
+function styles() {
+  return gulp.src('public/styles/**/*.scss')
     .pipe(sass())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/assets/css'));
-});
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public/assets/css'))
+    .on('error', (err) => {
+      console.log(err)
+      process.exit(1)
+    })
+}
+
+function watch() {
+  gulp.watch('public/styles/**/*.scss', styles);
+}
+
+exports.styles = styles;
+exports.watch = watch;
+
+gulp.task('default', watch);
+gulp.task('styles', styles);
